14f2d64ffde3ba1488ebc56395f1183e7c1c73cb
ChristianAntun1 <14216745-ChristianAntun1@users.noreply.replit.com>
2023-06-29 19:09:09 +0000

    Initial commit

---
+++ hpp/FigGeometrica.hpp
@@ -0,0 +1,22 @@
+#ifndef FIGGEOMETRICA_HPP
+#define FIGGEOMETRICA_HPP
+
+class FiguraGeometrica{
+public:
+    FiguraGeometrica();
+    // FUNCAO VIRTUAL PURA TORNA A CLASSE ABSTRATA
+    virtual void draw()=0;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //SCULPTOR_HPP
---
+++ hpp/putVoxel.hpp
@@ -0,0 +1,10 @@
+#ifndef PUTVOXEL_HPP
+#define PUTVOXEL_HPP
+
+
+
+
+
+
+
+#endif //SCULPTOR_HPP
